0e80ab7b06879a74a2fe0ab99e83f724ae49de7d
Kylejeong2 <Kylejeong21@gmail.com>
2024-08-04 10:25:33 -0700

    fixed height adj on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,7 +18,7 @@ const DashboardPage = async (props: Props) => {
     <>
         <div className='min-h-screen'>
             <div className='max-w-7xl mx-auto p-10'>
-                <div className='h-14'>
+                <div className='min-h-screen'>
                     <div className='flex justify-between items-center md:flex-row flex-col'>
                         <div className='flex items-center'>
                             <Link href="/">
